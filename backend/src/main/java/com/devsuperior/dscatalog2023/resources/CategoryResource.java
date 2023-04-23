938fe37f41063209bc33d897dbf52a8a999a727a
Robert Teixeira <michel.atlantico@gmail.com>
2023-04-23 18:38:26 -0300

    insert post category

--- backend/src/main/java/com/devsuperior/dscatalog2023/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog2023/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog2023.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import com.devsuperior.dscatalog2023.dto.CategoryDTO;
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * Class that implements the REST API
@@ -32,6 +36,14 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
 		CategoryDTO dto = service.findByID(id);
 		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+
 	}
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog2023/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog2023/services/CategoryService.java
@@ -34,4 +34,12 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		Category savedEntity = repository.save(entity);
+		return new CategoryDTO(savedEntity);
+	}
+
 }
