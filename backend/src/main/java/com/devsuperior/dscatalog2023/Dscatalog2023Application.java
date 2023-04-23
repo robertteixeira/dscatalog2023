dc57885fae6133403d05151b3a2948e1f07fd747
Robert Teixeira <michel.atlantico@gmail.com>
2023-04-23 13:47:48 -0300

    backend project created

---
+++ backend/src/main/java/com/devsuperior/dscatalog2023/Dscatalog2023Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscatalog2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscatalog2023Application.class, args);
+	}
+
+}
